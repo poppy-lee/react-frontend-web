800833a01d2ed8f56987d77e975fbb761d138a9d
strotend <strotend@gmail.com>
2019-07-11 10:48:50 +0900

    react-hot-loader setConfig 제거

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,10 @@ import "whatwg-fetch"
 import "@assets/stylesheet.css"
 
 import React from "react"
-import { setConfig } from "react-hot-loader"
 import { hot } from "react-hot-loader/root"
 
 import Router from "./Router"
 
 const App: React.FunctionComponent = () => <Router />
-setConfig({ logLevel: "none" })
 
 export default hot(App)
